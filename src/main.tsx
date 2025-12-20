67a0723cd8b05a096fd84ab8e6db2690183426c9
Rob Higgins <higginsrob@gmail.com>
2025-12-20 08:48:41 -0700

    add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,16 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { store } from "./app/store";
 
 const elem = document.getElementById("root")!;
 const app = (
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
 
