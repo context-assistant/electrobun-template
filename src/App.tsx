2cc9af989c6f942eb7d5e6b4caa60caff125f6e9
Rob Higgins <higginsrob@gmail.com>
2025-12-20 08:36:31 -0700

    remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { APITester } from "./APITester";
 import "./index.css";
 
 import logo from "./design/logo.svg";
-import reactLogo from "./react.svg";
 
 export function App() {
   return (
@@ -15,18 +12,6 @@ export function App() {
           className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
         />
       </div>
-      <Card>
-        <CardHeader className="gap-4">
-          <CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-          <CardDescription>
-            Edit <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">src/App.tsx</code> and save to
-            test HMR
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <APITester />
-        </CardContent>
-      </Card>
     </div>
   );
 }
